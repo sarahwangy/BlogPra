abc47adce72f47e22399b99b766eafd6c2c49be6
Sarah <sarahwangdk@gmail.com>
2022-05-13 12:27:34 +1000

    change small detail

--- src/components/homeComponent/homeList.js
+++ src/components/homeComponent/homeList.js
@@ -1,69 +1,59 @@
-import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { ListItem, ListInfo, LoadMore } from '../../pages/home/styled'
-import { getMoreListAction } from '../../redux/action/homeAction'
-
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { ListItem, ListInfo, LoadMore } from "../../pages/home/styled";
+import { getMoreListAction } from "../../redux/action/homeAction";
 
 class HomeList extends PureComponent {
-    render() {
+  render() {
+    // 这里为什么是空的？
+    console.log("home list props:", this.props);
 
-        // 这里为什么是空的？ 
-        console.log("home list props:", this.props)
+    return (
+      <div>
+        {this.props.articleList.map((articleObj, index) => {
+          console.log("articleObj.title,", articleObj.title);
+          console.log(JSON.stringify(articleObj));
 
-        return (
-            <div>
-
-                {
-                    this.props.articleList.map((articleObj, index) => {
-                        console.log("articleObj.title,", articleObj.title)
-                        console.log(JSON.stringify(articleObj))
-
-                        return (
-                            <Link key={index} to={`/detail/${articleObj.id}`} >
-                                <ListItem >
-                                    <img className='list-img' src={articleObj.imgUrl} alt='120' />
-                                    <ListInfo>
-                                        <h3 className='list-title'>
-                                            {articleObj.title}
-                                        </h3>
-                                        <p className='list-desc'>
-                                            {articleObj.desc}
-                                        </p>
-                                    </ListInfo>
-                                </ListItem>
-                            </Link>
-                        )
-
-                    })
-                }
-                <LoadMore onClick={() => this.props.getMoreList(this.props.articleListPage)}> 加载更多</LoadMore>
-            </div >
-        )
-    }
+          return (
+            <Link key={index} to={`/detail/${articleObj.id}`}>
+              <ListItem>
+                <img className="list-img" src={articleObj.imgUrl} alt="120" />
+                <ListInfo>
+                  <h3 className="list-title">{articleObj.title}</h3>
+                  <p className="list-desc">{articleObj.desc}</p>
+                </ListInfo>
+              </ListItem>
+            </Link>
+          );
+        })}
+        <LoadMore
+          onClick={() => this.props.getMoreList(this.props.articleListPage)}
+        >
+          Load More
+        </LoadMore>
+      </div>
+    );
+  }
 }
-
-
 
 const mapToPropsState = (state) => {
-    console.log("mapToPropsState --->")
-    console.log(JSON.stringify(state.home.articleListPage))
-    // console.log(JSON.stringify(state.home.articleList))
-    return {
-        articleList: state.home.articleList,
-        articleListPage: state.home.articleListPage
-    }
-
-}
+  console.log("mapToPropsState --->");
+  console.log(JSON.stringify(state.home.articleListPage));
+  // console.log(JSON.stringify(state.home.articleList))
+  return {
+    articleList: state.home.articleList,
+    articleListPage: state.home.articleListPage,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
-    // console.log("dispatch")
-    return {
-        getMoreList: (articleListPage) => {
-            dispatch(getMoreListAction(articleListPage))
-        }
-    }
-}
+  // console.log("dispatch")
+  return {
+    getMoreList: (articleListPage) => {
+      dispatch(getMoreListAction(articleListPage));
+    },
+  };
+};
 
-
-export default connect(mapToPropsState, mapDispatchToProps)(HomeList)
+export default connect(mapToPropsState, mapDispatchToProps)(HomeList);
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,112 +1,113 @@
-import React, { PureComponent } from 'react'
-import { connect } from 'react-redux'
-import { GetHomeAction, changeShowSroll } from '../../redux/action/homeAction'
+import React, { PureComponent } from "react";
+import { connect } from "react-redux";
+import { GetHomeAction, changeShowSroll } from "../../redux/action/homeAction";
 
 // 只引入一个index 文件，然后 把之前暴露的 export的都 解析出来。。使用
-import { Topic, Write, Recommend, HomeList } from '../../components/homeComponent/index'
-
+import {
+  Topic,
+  Write,
+  Recommend,
+  HomeList,
+} from "../../components/homeComponent/index";
 
 // import Recommend from '../../components/homeComponent/recommend'
 // import Topic from '../../components/homeComponent/topic'
 // import Write from '../../components/homeComponent/write'
-import { HomeWrapper, HomeLeft, HomeRight, BackTop, WriteWrapper } from './styled'
+import {
+  HomeWrapper,
+  HomeLeft,
+  HomeRight,
+  BackTop,
+  WriteWrapper,
+} from "./styled";
 // import HomeList from '../../components/homeComponent/homeList'
 
-
 class Home extends PureComponent {
+  handleScrollTop = () => {
+    window.scrollTo(0, 0);
+  };
 
-    handleScrollTop = () => {
-        window.scrollTo(0, 0);
-    }
+  //  绑定事件， 监听 scroll， 用户是否开始 scroll页面
+  bindEvent = () => {
+    window.addEventListener("scroll", this.props.changeShowSroll);
+  };
 
-    //  绑定事件， 监听 scroll， 用户是否开始 scroll页面
-    bindEvent = () => {
-        window.addEventListener('scroll', this.props.changeShowSroll)
-    }
+  //  componentDid Mount 挂载的时候调用 bindEvent, this event 直接监听 changesroll方法
+  //  立马加载，但是用户没有scroll 情况下，也不会立马执行这个钩子
+  componentDidMount() {
+    this.bindEvent();
 
+    // 借助 redux dev tool 查看 state 状态，哪些获取了，哪些没有获取。。
+    //  如果没有其他按钮绑定 这个axios 调取数据的话，可以用 componentdidmount 调取changelist 方发
+    this.props.changeHomeList();
+  }
 
-    //  componentDid Mount 挂载的时候调用 bindEvent, this event 直接监听 changesroll方法
-    //  立马加载，但是用户没有scroll 情况下，也不会立马执行这个钩子
-    componentDidMount() {
-        this.bindEvent();
+  //  取消监听事件：
 
-        // 借助 redux dev tool 查看 state 状态，哪些获取了，哪些没有获取。。
-        //  如果没有其他按钮绑定 这个axios 调取数据的话，可以用 componentdidmount 调取changelist 方发
-        this.props.changeHomeList();
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.props.changeShowSroll);
+  }
 
-    }
+  render() {
+    return (
+      <HomeWrapper>
+        <HomeLeft>
+          <img
+            className="banner-img"
+            src="https://upload.jianshu.io/admin_banners/web_images/5055/348f9e194f4062a17f587e2963b7feb0b0a5a982.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540"
+            alt="540"
+          />
+          <Topic />
+          <HomeList />
+        </HomeLeft>
 
+        <HomeRight>
+          <Recommend />
+          <WriteWrapper>
+            <Write />
+          </WriteWrapper>
+        </HomeRight>
+        {this.props.showScroll ? (
+          <BackTop onClick={this.handleScrollTop}>GoTop</BackTop>
+        ) : null}
 
-    //  取消监听事件：
-
-    componentWillUnmount() {
-        window.removeEventListener('scroll', this.props.changeShowSroll)
-    }
-
-
-    render() {
-        return (
-
-            <HomeWrapper >
-                <HomeLeft >
-                    <img className='banner-img' src="https://upload.jianshu.io/admin_banners/web_images/5055/348f9e194f4062a17f587e2963b7feb0b0a5a982.png?imageMogr2/auto-orient/strip|imageView2/1/w/1250/h/540" alt="540" />
-                    <Topic />
-                    <HomeList />
-                </HomeLeft>
-
-                <HomeRight>
-                    <Recommend />
-                    <WriteWrapper>
-                        <Write />
-                    </WriteWrapper>
-
-                </HomeRight>
-                {
-                    this.props.showScroll ? <BackTop onClick={this.handleScrollTop}>回到顶部</BackTop> : null
-                }
-
-                {/* 可以用按钮 绑定事件，但是这样有点多余，用户滑到 比较多的时候，页面可以scroll 的时候，生命周期钩子 自动使 回到顶部 出现，用户点击 回到顶部，直接回到最上面 top*/}
-                {/* <button onClick={this.bindEvent}> click </button> */}
-            </HomeWrapper>
-
-
-        )
-    }
+        {/* 可以用按钮 绑定事件，但是这样有点多余，用户滑到 比较多的时候，页面可以scroll 的时候，生命周期钩子 自动使 回到顶部 出现，用户点击 回到顶部，直接回到最上面 top*/}
+        {/* <button onClick={this.bindEvent}> click </button> */}
+      </HomeWrapper>
+    );
+  }
 }
 
 //container home 里面不用获取state吗，谁用谁自己获取，反正大家都用connect了， home用哪个state，就只取哪个state？
 
 const mapStateToProps = (state) => ({
-    // articleList: state.home.articleList,
-    // topicList: state.home.topicList,
-    // recommendList: state.home.recommendList
-    showScroll: state.home.showScroll
+  // articleList: state.home.articleList,
+  // topicList: state.home.topicList,
+  // recommendList: state.home.recommendList
+  showScroll: state.home.showScroll,
 });
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        changeHomeList: () => dispatch(GetHomeAction()),
-        // 回到顶部，/???
-        // changeShowSroll: () => {
-        //     console.log("scrolll--->", document.documentElement.scrollTop)
+  return {
+    changeHomeList: () => dispatch(GetHomeAction()),
+    // 回到顶部，/???
+    // changeShowSroll: () => {
+    //     console.log("scrolll--->", document.documentElement.scrollTop)
 
-        // }
-        changeShowSroll: () => {
+    // }
+    changeShowSroll: () => {
+      //  打 log 排查 错误 - 是否目前scroll 大于400
+      console.log("scrolll--->", document.documentElement.scrollTop);
+      //判断是否 是 TRUE值，是TRUE的话，执行那个dispatch。。。
+      console.log(document.documentElement.scrollTop > 400);
 
-            //  打 log 排查 错误 - 是否目前scroll 大于400
-            console.log("scrolll--->", document.documentElement.scrollTop)
-            //判断是否 是 TRUE值，是TRUE的话，执行那个dispatch。。。
-            console.log(document.documentElement.scrollTop > 400)
+      if (document.documentElement.scrollTop > 400) {
+        dispatch(changeShowSroll(true));
+      } else {
+        dispatch(changeShowSroll(false));
+      }
+    },
+  };
+};
 
-            if (document.documentElement.scrollTop > 400) {
-                dispatch(changeShowSroll(true))
-            } else {
-                dispatch(changeShowSroll(false))
-            }
-        }
-    }
-
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
-
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
